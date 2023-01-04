f96c7d3f98a96811fd9e83228622326d93f65acd
511844767 <90088954+511844767@users.noreply.github.com>
2023-01-04 10:13:01 +0800

    增加纳秒级延迟

--- SYSTEM/delay/delay.h
+++ SYSTEM/delay/delay.h
@@ -6,6 +6,48 @@ void delay_init(void);
 void delay_ms(u16 nms);
 void delay_us(u32 nus);
 
+/**
+ * @brief 纳秒级延时（72MHz）
+ * @note __NOP消耗1个时钟周期（72MHz下为13.888ns），这些延迟的误差为-1ns~0ns
+ * 
+ */
+#define delay_14ns(){ \
+    __NOP(); \
+}   // 1 * NOP
+#define delay_55ns(){ \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+}   // 4 * NOP
+#define delay_111ns(){ \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+}   // 8 * NOP
+#define delay_208ns(){ \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+    __NOP(); \
+}   // 15 * NOP
+
 #endif
 
 
